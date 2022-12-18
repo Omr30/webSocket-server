28a6979c8ef050b70c0d1cd03a9c62ba89ce7ec2
mvrk <oscarmenacho3@gmail.com>
2022-12-18 00:32:17 -0500

    Fundamentos sobre Web Sockets

---
+++ models/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+
+
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        this.paths = { }
+
+        // Middlewares
+
+        this.middlewares();
+
+        // Rutas de mi app
+
+        this.routes();
+    }
+
+    middlewares() {
+
+        // CORS
+        this.app.use( cors() );
+
+        // Directorio Publico
+        this.app.use( express.static('public') );
+
+    }
+
+    routes() {
+        // this.app.use( this.paths.auth, require('../routes/auth') );
+    }
+
+    listen() {
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en puerto ', this.port)
+        });
+    }
+
+}
+
+module.exports = Server;
