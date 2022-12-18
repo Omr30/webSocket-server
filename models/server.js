4a5e6eecdad633af9499f2723b10c26e9734e6ef
mvrk <oscarmenacho3@gmail.com>
2022-12-18 01:36:16 -0500

    Broadcast - Ordenar nuestro código

--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { socketController } = require('../sockets/controller');
 
 
 
@@ -40,21 +41,7 @@ class Server {
     }
 
     sockets() {
-        this.io.on('connection', socket => {
-            // console.log('cliente conectado', socket.id);
-
-            socket.on('disconnect', () =>{
-                // console.log('Cliente desconectado', socket.id);
-            })
-
-            socket.on('enviar-mensaje', (payload, callback) => {
-
-                const id = 123456;
-                callback(id);
-                // this.io.emit('enviar-mensaje', payload)
-            })
-
-        })
+        this.io.on('connection', socketController)
     }
 
     listen() {
---
+++ sockets/controller.js
@@ -0,0 +1,23 @@
+
+
+
+const socketController = (socket) => {
+    
+        console.log('cliente conectado', socket.id);
+
+        socket.on('disconnect', () =>{
+            console.log('Cliente desconectado', socket.id);
+        })
+
+        socket.on('enviar-mensaje', (payload, callback) => {
+
+            const id = 123456;
+            callback(id);
+
+            socket.broadcast.emit('enviar-mensaje', payload)
+        })
+}
+
+module.exports = {
+    socketController
+}
