d100332ceb04a73328c8dfff573aa9a9b353e230
mvrk <oscarmenacho3@gmail.com>
2022-12-18 01:00:40 -0500

    Mensajes de conexcion y desconexion - Cliente

--- public/js/socket-cliente.js
+++ public/js/socket-cliente.js
@@ -1,3 +1,19 @@
-
+// Referenmcias del HTML
+const lblOnline = document.querySelector('#lblOnline')
+const lblOffline = document.querySelector('#lblOffline')
 
 const socket = io()
+
+socket.on('connect', () => {
+    console.log('Conectado');
+
+    lblOffline.style.display = 'none'
+    lblOnline.style.display = ''
+})
+
+socket.on('disconnect', () => {
+    console.log('Desconectado del servidor');
+
+    lblOffline.style.display = ''
+    lblOnline.style.display = 'none'
+})
