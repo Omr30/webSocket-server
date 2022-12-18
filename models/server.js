4466ca17ba261bc8066260d48fbee5601e37ca4d
mvrk <oscarmenacho3@gmail.com>
2022-12-18 00:42:40 -0500

    Instalacion de socket.io

--- models/server.js
+++ models/server.js
@@ -8,6 +8,8 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.server = require('http').createServer(this.app)
+        this.io = require('socket.io')(this.server)
 
         this.paths = { }
 
@@ -35,7 +37,7 @@ class Server {
     }
 
     listen() {
-        this.app.listen( this.port, () => {
+        this.server.listen( this.port, () => {
             console.log('Servidor corriendo en puerto ', this.port)
         });
     }
