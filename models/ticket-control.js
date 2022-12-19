47bbfd98a7df582f216435dbfdd27e4a696c27d0
mvrk <oscarmenacho3@gmail.com>
2022-12-19 17:36:24 -0500

    Modelo - siguiente y atender ticket

--- models/ticket-control.js
+++ models/ticket-control.js
@@ -1,7 +1,12 @@
 const path = require('path')
 const fs = require('fs')
  
-
+class Ticket {
+    constructor(numero, escritorio){
+        this.numero = numero
+        this.escritorio = escritorio
+    }
+}
 
 class TicketControl {
     constructor(){
@@ -38,6 +43,35 @@ class TicketControl {
     guardarDB() {
         const dbPath = path.join(__dirname, '../db/data.json')
         fs.writeFileSync(dbPath, JSON.stringify(this.toJson))
+    }
+
+    siguiente() {
+        this.ultimo += 1
+        const ticket = new Ticket(this.ultimo, null)
+        this.tickets.push(ticket)
+
+        this.guardarDB()
+        return 'Ticket ' + this.numero
+    }
+
+    atenderTicket(escritorio){
+        // No tenemos tickets
+        if(this.tickets.length === 0){
+            return null
+        }
+
+        const ticket = this.tickets.shift() // this.tickets[0]
+        ticket.escritorio = escritorio
+
+        this.ultimos4.unshift(ticket)
+
+        if(this.ultimos4.length > 4){
+            this.ultimos4.splice(-1,1)
+        }
+
+        this.guardarDB();
+        
+        return ticket
     }
 
 }
