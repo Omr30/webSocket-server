7fd29a754ebc68d0ac3e4daf0bec6a250fb2de10
mvrk <oscarmenacho3@gmail.com>
2022-12-20 01:04:38 -0500

    Proparando pantalla de inicio

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,38 @@
 
+const lblEscritorio = document.querySelector('h1')
+const btnAtender = document.querySelector('button')
 
-console.log('Escritorio HTML');
+
+const searchParams = new URLSearchParams(window.location.search)
+
+if(!searchParams.has('escritorio')){
+    window.location = 'index.html'
+    throw new Error('El escritorio es obligatorio')
+}
+
+const escritorio = searchParams.get('escritorio')
+lblEscritorio.innerHTML = escritorio
+
+const socket = io();
+
+socket.on('connect', () => {
+    // console.log('Conectado');
+    btnAtender.disabled = false
+});
+
+socket.on('disconnect', () => {
+    // console.log('Desconectado del servidor');
+    btnAtender.disabled = true
+})
+
+socket.on('ultimo-ticket', (ultimo) => {
+    // lblNuevoTicket.innerText = `Ticket: ${ultimo}`
+})
+
+btnAtender.addEventListener( 'click', () => {
+    
+    // socket.emit( 'siguiente-ticket', null, ( ticket ) => {
+    //     lblNuevoTicket.innerText = ticket
+    // });
+
+});
