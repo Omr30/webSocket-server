e0bbca07e5ed98c34c50a6f3a0c88176ce74cdcc
mvrk <oscarmenacho3@gmail.com>
2022-12-20 02:19:18 -0500

    Tickets pendientes por atender

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -3,6 +3,7 @@ const lblEscritorio = document.querySelector('h1')
 const btnAtender = document.querySelector('button')
 const lblTicket = document.querySelector('small')
 const divAlerta = document.querySelector('.alert')
+const lblPendientes = document.querySelector('#lblPendientes')
 
 
 const searchParams = new URLSearchParams(window.location.search)
@@ -31,6 +32,15 @@ socket.on('disconnect', () => {
 
 socket.on('ultimo-ticket', (ultimo) => {
     // lblNuevoTicket.innerText = `Ticket: ${ultimo}`
+})
+
+socket.on('tickets-pendientes', (pendientes) => {
+    if(pendientes === 0){
+        lblPendientes.style.display = 'none'
+    }else {
+        lblPendientes.style.display = ''
+        lblPendientes.innerHTML = pendientes
+    }
 })
 
 btnAtender.addEventListener( 'click', () => {
--- sockets/controller.js
+++ sockets/controller.js
@@ -4,15 +4,16 @@ const ticketControl = new TicketControl()
 
 const socketController = (socket) => {
 
+    // Cuando un cliente se conecta
     socket.emit('ultimo-ticket', ticketControl.ultimo)
     socket.emit('estado-actual', ticketControl.ultimos4)
+    socket.emit('tickets-pendientes', ticketControl.tickets.length)
 
     socket.on('siguiente-ticket', (payload, callback) => {
 
         const siguiente = ticketControl.siguiente();
         callback(siguiente);
-
-        // TODO: Notificar que hay un nuevo ticket pendiente de asignar
+        socket.broadcast.emit('tickets-pendientes', ticketControl.tickets.length)
     })
 
     socket.on('atender-ticket', ({escritorio}, callback) => {
@@ -27,6 +28,9 @@ const socketController = (socket) => {
 
         // TODO: Notificar cambio en los ultimos4
         socket.broadcast.emit('estado-actual', ticketControl.ultimos4)
+        socket.emit('tickets-pendientes', ticketControl.tickets.length)
+        socket.broadcast.emit('tickets-pendientes', ticketControl.tickets.length)
+
 
         if(!ticket){
             callback({
