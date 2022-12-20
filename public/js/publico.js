af29bce46920c5ee37cdb1af66b51e8ec7a61fa7
mvrk <oscarmenacho3@gmail.com>
2022-12-20 01:49:23 -0500

    Mostrar cola de ticket en pantalla

--- public/js/publico.js
+++ public/js/publico.js
@@ -1,3 +1,38 @@
 
+const lblTicket1 = document.querySelector('#lblTicket1')
+const lblEscritorio1 = document.querySelector('#lblEscritorio1')
+const lblTicket2 = document.querySelector('#lblTicket2')
+const lblEscritorio2 = document.querySelector('#lblEscritorio2')
+const lblTicket3 = document.querySelector('#lblTicket3')
+const lblEscritorio3 = document.querySelector('#lblEscritorio3')
+const lblTicket4 = document.querySelector('#lblTicket4')
+const lblEscritorio4 = document.querySelector('#lblEscritorio4')
 
-console.log('Público HTML')
+const socket = io();
+
+socket.on('estado-actual', (payload) => {
+
+    const [ticket1, ticket2, ticket3, ticket4] = payload
+
+    if(ticket1){
+        lblTicket1.innerHTML = `Ticket ${ticket1.numero}`
+        lblEscritorio1.innerHTML = ticket1.escritorio
+        
+    }
+    if(ticket2){
+        lblTicket2.innerHTML = `Ticket ${ticket2.numero}`
+        lblEscritorio2.innerHTML = ticket2.escritorio
+
+    }
+    if(ticket3){
+        lblTicket3.innerHTML = `Ticket ${ticket3.numero}`
+        lblEscritorio3.innerHTML = ticket3.escritorio
+
+    }
+    if(ticket4){
+        lblTicket4.innerHTML = `Ticket ${ticket4.numero}`
+        lblEscritorio4.innerHTML = ticket4.escritorio
+
+    }
+
+})
--- sockets/controller.js
+++ sockets/controller.js
@@ -5,6 +5,7 @@ const ticketControl = new TicketControl()
 const socketController = (socket) => {
 
     socket.emit('ultimo-ticket', ticketControl.ultimo)
+    socket.emit('estado-actual', ticketControl.ultimos4)
 
     socket.on('siguiente-ticket', (payload, callback) => {
 
@@ -25,8 +26,7 @@ const socketController = (socket) => {
         const ticket = ticketControl.atenderTicket(escritorio)
 
         // TODO: Notificar cambio en los ultimos4
-
-
+        socket.broadcast.emit('estado-actual', ticketControl.ultimos4)
 
         if(!ticket){
             callback({
